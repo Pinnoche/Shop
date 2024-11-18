fd70841e9462095b897d4e31a44b2a860db74a39
Pinnoche <ptime552@gmail.com>
2024-11-18 01:05:10 +0100

    react-slick added to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Welcome from './Pages/Welcome'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 function App() {
 
   return (
