8670fb3bb5e1596593564af0102051593eb39e70
Pinnoche <ptime552@gmail.com>
2024-11-18 01:25:37 +0100

    framer-motion

--- frontend/src/Pages/Welcome.jsx
+++ frontend/src/Pages/Welcome.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react"
-import RegOrLogin from "../components/RegOrLogin"
-function Welcome() {
-  useEffect ( () => {
 
-  }, [])
+import RegOrLogin from "../components/RegOrLogin"
+import Slider from "react-slick"
+function Welcome() {
+  
   return (
     <div className="bg-black text-white w-full h-screen p-8">
       <p>Welcome Page</p>
       <RegOrLogin />
+      <Slider>
+        <div></div>
+      </Slider>
     </div>
   )
 }
