4f7ee05aeba981f3b83990b7c032f6ac50a6e8c9
Pinnoche <ptime552@gmail.com>
2024-11-19 17:29:36 +0100

    slides

---
+++ frontend/src/components/NextArrow.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+function NextArrow({ onClick, slideCount, currentSlide, slidesToShow }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-300 w-12 h-12 rounded-full flex items-center justify-center text-black z-50 hover:bg-gray-500 ${
+        currentSlide === slideCount - slidesToShow ? "hidden" : "block"
+      }`}
+    >
+      →
+    </button>
+  );
+}
+
+NextArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  slideCount: PropTypes.number.isRequired,
+  currentSlide: PropTypes.number.isRequired,
+  slidesToShow: PropTypes.number.isRequired,
+};
+
+export default NextArrow;
