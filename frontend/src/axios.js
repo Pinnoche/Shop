b135f65f08bd08e3a952c67a0ee6bf8b893ce4bf
Pinnoche <ptime552@gmail.com>
2025-01-03 23:43:39 +0100

    docker

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-
 axios.defaults.baseURL = 'http://localhost:8580';
 
 export default axios;
--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,23 +1,26 @@
-import deals from "../deals.json"
+import deals from "../deals.json";
 
 function Category() {
   console.log(deals);
   return (
-    <div className="bg-gray-400 p-6 mt-20 rounded-md">
-      <h2 className="text-white text-2xl font-semibold mb-6 text-center">Category</h2>
+    <div className="bg-gray-400 mt-20 rounded-md overflow-hidden">
+      <div className="p-4 bg-red-500">
+        <h2 className="text-white text-2xl font-semibold mb-6 text-center">
+          Category
+        </h2>
+      </div>
       <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
         {deals.map((deal, index) => (
           <div
             key={index}
             className="w-full h-full relative bg-white shadow-md hover:shadow-gray-400 overflow-hidden hover:scale-105 transition-transform duration-300"
           >
-            
-              <img
-                src={deal.image}
-                alt={deal.title}
-                className="h-full w-full object-fill"
-              />
-      
+            <img
+              src={deal.image}
+              alt={deal.title}
+              className="h-full w-full object-fill"
+            />
+
             <div className="h-10 absolute truncate top-[80%] xl:top-[85%] inset-0 w-full bg-black bg-opacity-70 ">
               <p className="text-center sm:font-medium font-normal sm:text-sm lg:text-lg text-xs text-white">
                 {deal.title}
@@ -31,4 +34,3 @@ function Category() {
 }
 
 export default Category;
- 
